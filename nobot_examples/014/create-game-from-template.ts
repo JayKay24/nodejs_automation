62a656b5c50ecf7109c68a1b202c3f58c335326a
James Njuguna <jameskinyua590@gmail.com>
2020-12-08 14:18:08 +0300

    create a tool that creates a new game foundation by copying a game template

---
+++ nobot_examples/014/create-game-from-template.ts
@@ -0,0 +1,43 @@
+import path from "path";
+
+import colors from "colors";
+import readLineSync from "readline-sync";
+import fse from "fs-extra";
+
+const GAME_TEMPLATES = "game-templates";
+const NO_CHOICE_MADE = -1;
+const { green, cyan } = colors;
+
+// 1. Use a game template already built
+const templatesDir = path.join(__dirname, GAME_TEMPLATES);
+const templates = fse.readdirSync(templatesDir);
+
+const index = readLineSync.keyInSelect(templates);
+
+if (index === NO_CHOICE_MADE) {
+  process.exit(0);
+}
+
+// 2. Create a new project reskin based on our template
+const projectName = readLineSync.question("What is the name of your game? ", {
+  limit: (input) => input.trim().length > 0,
+  limitMessage: "The project has to have a name, try again",
+});
+
+const confirmCreateDirectory = readLineSync.keyInYN(
+  `You entered '${projectName}', create directory with this name? `
+);
+
+// 3. If happy to create, copy the template to the new location
+if (confirmCreateDirectory) {
+  const template = templates[index];
+  const src = path.join(templatesDir, template);
+  const destination = path.join(__dirname, projectName);
+
+  fse
+    .copy(src, destination)
+    .then(() => console.log(green(`Successfully created ${destination}`)))
+    .catch((err) => console.error(err));
+} else {
+  console.log(cyan("Aborted creating a new game"));
+}
